07cb1648320c7077c22dc2f4e55d4d20a29ae030
ShashankRaoCoding <Shashank.JRao@Outlook.Com>
2025-05-09 20:14:32 +0100

    Update public folder - 2025-05-09\_2014

--- Website-Resources/Javascript/wandermeadow.js
+++ Website-Resources/Javascript/wandermeadow.js
@@ -6,7 +6,7 @@ document.head.appendChild(link);
 const currentDomain = window.location.hostname;
 
 if (currentDomain === 'www.gwasexplorer.com') {
-    window.location.href = 'https://www.gwasexplorer.com/Projects/GWAS-Explorer/'; // Redirect to the page for www.gwasexplorer.com visitors
+    window.location.href = 'https://www.gwasexplorer.com/Projects/GWAS-Explorer'; // Redirect to the page for www.gwasexplorer.com visitors
 }
 
 function redirect(url) {
